428707a855287b69d78062731fb1be9a9d030f3c
MLPPsussex <113170604+MLPPsussex@users.noreply.github.com>
2023-05-18 08:16:05 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,10 +39,7 @@ export default function Home() {
             This is an about page
           </p>
           <p className="text-gray-600">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
+            Second block of text
           </p>
         </div>
       </div>
